8d2420989da83530b5cc5a0bdf588e2bdf8390a9
Jouni Juntunen <jouni.juntunen@oamk.fi>
2024-09-25 19:26:42 +0300

    Markkerin lisäys.

--- screens/Map.js
+++ screens/Map.js
@@ -1,17 +1,32 @@
 import { StyleSheet, SafeAreaView, Platform } from 'react-native'
 import React, { useState } from 'react'
-import MapView from 'react-native-maps'
+import MapView, { Marker } from 'react-native-maps'
 
 
 export default function Map({location}) {
-  
+  const [marker, setMarker] = useState(null)
+
+  const showMarker = (e) => {
+    const coords = e.nativeEvent.coordinate
+    setMarker(coords)
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <MapView 
         style={styles.map}
         region={location}
         mapType='satellite'
-      />
+        onLongPress={showMarker}
+      >
+        {
+          marker && 
+            <Marker 
+              title="My marker"
+              coordinate={{latitude: marker.latitude,longitude: marker.longitude}}
+            />
+        }
+      </MapView>
     </SafeAreaView>
   )
 }
